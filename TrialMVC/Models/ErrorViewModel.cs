2f6931941b473f8321dd0438677aba7bcef99f13
larioh <kemboihillz@gmail.com>
2021-10-08 14:35:41 +0300

    Initial Commit

---
+++ TrialMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TrialMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
